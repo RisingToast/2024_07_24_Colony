7747e94bef0df69b4d823958c9c4ea88af5dcb42
RisingToast <6267134@naver.com>
2024-07-29 17:35:38 +0900

    0729

--- python/model_load.py
+++ python/model_load.py
@@ -1,10 +1,19 @@
+import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
 # 학습된 모델 로드
-model = YOLO('C:/kkt/2024_07_24_Colony/save_model/train_10_300_64_500.pt')
+model = YOLO('C:/kkt/2024_07_24_Colony/save_model/train_50_300_16_500.pt')
 
 # 이미지 예측
-results = model.predict(source='C:/kkt/2024_07_24_Colony/IMG2/51.cropped-aureus-colony-counting.jpg', save=True)
+results = model.predict(source='C:/kkt/2024_07_24_Colony/IMG2/', save=True)
 
 # 예측 결과 확인
 print(results)
+
+# 바운딩 박스 개수 세기 및 이미지 출력
+for result in results:
+    count = len(result.boxes)  # 각 이미지에서 바운딩 박스의 수 세기
+    print(f'예측된 바운딩 박스의 개수: {count}')
+    
+    # 예측된 바운딩 박스가 포함된 이미지 보기
+    ##result.show()  # 결과를 이미지로 띄움
--- python/test.py
+++ python/test.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # 모델 학습
-model.train(data='C:/kkt/2024_07_24_Colony/roboflow_dataset/data.yaml', epochs = 5, patience = 300, batch = 16, imgsz = 500)
+model.train(data='C:/kkt/2024_07_24_Colony/roboflow_dataset/data.yaml', epochs = 50, patience = 300, batch = 16, imgsz = 500)
 
 # 학습 모델 확인
 print(type(model.names), len(model.names))
@@ -15,6 +15,6 @@ print(model.names)
 results = model.predict(source = 'C:/kkt/2024_07_24_Colony/IMG2/', save = True)
 
 # 모델 저장
-model.save('C:/kkt/2024_07_24_Colony/save_model/train_5_300_16_500.pt')
+model.save('C:/kkt/2024_07_24_Colony/save_model/train_50_300_16_500.pt')
 
 

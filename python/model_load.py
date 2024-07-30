95d361ed4c7d0a67ad46993477010b830d2143aa
RisingToast <6267134@naver.com>
2024-07-30 16:58:07 +0900

    0730

--- python/model_load.py
+++ python/model_load.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
+import cv2
+import os
+from matplotlib import pyplot as plt
 from ultralytics import YOLO
 
 # 학습된 모델 로드
-model = YOLO('C:/kkt/2024_07_24_Colony/save_model/train_50_300_16_500.pt')
+model = YOLO('C:/kkt/2024_07_24_Colony/save_model/train_150_300_16_500.pt')
 
 # 이미지 예측
 results = model.predict(source='C:/kkt/2024_07_24_Colony/IMG2/', save=True)
@@ -10,10 +12,25 @@ results = model.predict(source='C:/kkt/2024_07_24_Colony/IMG2/', save=True)
 # 예측 결과 확인
 print(results)
 
-# 바운딩 박스 개수 세기 및 이미지 출력
+# 바운딩 박스 개수 세기 및 이미지 저장
+output_dir = 'C:/kkt/2024_07_24_Colony/img_set/'
+os.makedirs(output_dir, exist_ok=True)
+
 for result in results:
     count = len(result.boxes)  # 각 이미지에서 바운딩 박스의 수 세기
     print(f'예측된 바운딩 박스의 개수: {count}')
     
-    # 예측된 바운딩 박스가 포함된 이미지 보기
-    ##result.show()  # 결과를 이미지로 띄움
+    # 원본 이미지 로드
+    image_path = result.path
+    image = cv2.imread(image_path)
+    
+    # 바운딩 박스 그리기
+    for box in result.boxes:
+        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)  # 각 바운딩 박스의 좌표를 추출하여 정수형으로 변환
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    
+    # 이미지 저장 (OpenCV BGR을 Matplotlib RGB로 변환)
+    save_path = os.path.join(output_dir, os.path.basename(image_path))
+    plt.imsave(save_path, cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+print("바운딩 박스가 그려진 이미지를 저장했습니다.")
